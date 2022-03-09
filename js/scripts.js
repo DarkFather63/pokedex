cf804793f30d24a12dbe801f2b3260c689ba452d
DarkFather63 <erynlcraig@gmail.com>
2022-03-09 08:36:08 -0800

    Changed file to store pokedex data
    Original file showed favoriteFood name and value - then document write. Changed whole file to start pokedex with an array of objects.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
-alert ('Hello World');
-let name = 'favoriteFood';
-let favoriteFood = 'chickenCurry';
-document.write(favoriteFood);
+let pokemonList = [
+{ name: 'Luxray', type: 'electric', height: 1.4} ,
+{ name: 'Zebstrika', type: 'electric', height: 1.6} ,
+{ name: 'Charizard', type: ['electric', 'flying'], height: 1.7} ,
+{ name: 'Gengar', type: ['ghost', 'poison' ], height: 1.4}
+]
