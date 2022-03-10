26386e1a9ab0403374b3ae5d5cf0f69721281985
DarkFather63 <erynlcraig@gmail.com>
2022-03-10 12:05:39 -0800

    Added loops and conditionals
    Added loop to list pokemon and height, plus a conditional for the largest pokemon. Changed heights to be clearer and differentiate/simplify code for now. Those will probably be changed back later once I have a better understanding of JS.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,15 @@
 let pokemonList = [
-{ name: 'Luxray', type: 'electric', height: 1.4} ,
-{ name: 'Zebstrika', type: 'electric', height: 1.6} ,
-{ name: 'Charizard', type: ['electric', 'flying'], height: 1.7} ,
-{ name: 'Gengar', type: ['ghost', 'poison' ], height: 1.4}
+{ name: 'Luxray', type: 'electric', height: 2} ,
+{ name: 'Zebstrika', type: 'electric', height: 2} ,
+{ name: 'Charizard', type: ['electric', 'flying'], height: 7} ,
+{ name: 'Gengar', type: ['ghost', 'poison' ], height: 3}
 ]
+//creates a data array storing pokemon data
+
+for (let i=0; i < pokemonList.length; i++)
+	if(pokemonList[i].height < 4){
+		document.write(pokemonList[i].name + '( height: ' + pokemonList[i].height + ')' )
+	} else if(pokemonList[i].height > 3){
+		document.write(pokemonList[i].name + '( height: ' + pokemonList[i].height + ')' + '-Wow, that\'s big!')
+	} 
+	//list of pokemon and their height, with largest noted under conditional
