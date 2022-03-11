744c2ff83ae6dd8e8984ef625ab2e53a087b3b8f
DarkFather63 <erynlcraig@gmail.com>
2022-03-11 12:53:08 -0800

    updated colors and some text decorations

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,16 @@
+//creates a data array storing pokemon data
 let pokemonList = [
 { name: 'Luxray', type: 'electric', height: 2} ,
 { name: 'Zebstrika', type: 'electric', height: 2} ,
 { name: 'Charizard', type: ['electric', 'flying'], height: 7} ,
 { name: 'Gengar', type: ['ghost', 'poison' ], height: 3}
-]
-//creates a data array storing pokemon data
+];
 
+
+//list of pokemon and their height, with largest noted under conditional
 for (let i=0; i < pokemonList.length; i++)
 	if(pokemonList[i].height < 4){
-		document.write(pokemonList[i].name + '( height: ' + pokemonList[i].height + ')')
+		document.write(pokemonList[i].name + '( height: ' + pokemonList[i].height + ')' + '<br/>')
 	} else if(pokemonList[i].height > 3){
-		document.write(pokemonList[i].name + '( height: ' + pokemonList[i].height + ')' + '-Wow, that\'s big!')
-	} 
-	//list of pokemon and their height, with largest noted under conditional
+		document.write(pokemonList[i].name + '( height: ' + pokemonList[i].height + ')' + '-Wow, that\'s big!' + '<br/>')
+	} ;
