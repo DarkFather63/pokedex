488d153994652bb7e97a88c4ca4ab3a8c32f5b76
DarkFather63 <erynlcraig@gmail.com>
2022-03-24 16:44:20 -0700

    Cleaned up IIFE and functions after review
    made getAll and add their own functions within the IIFE and corrected the forEach loop to make more sense.

--- js/scripts.js
+++ js/scripts.js
@@ -4,39 +4,36 @@
 let pokemonRepository = (function(){
 
 	let pokemonList = [
-	{ name: 'Luxray', type: 'electric', height: 2} ,
-	{ name: 'Zebstrika', type: 'electric', height: 2} ,
-	{ name: 'Charizard', type: ['electric', 'flying'], height: 7} ,
-	{ name: 'Gengar', type: ['ghost', 'poison' ], height: 3}
+	{ name: ' Luxray ', type: ' electric', height: 2} ,
+	{ name: ' Zebstrika ', type: ' electric', height: 2} ,
+	{ name: ' Charizard', type: [' electric', ' flying'], height: 7} ,
+	{ name: ' Gengar', type: [' ghost', ' poison' ], height: 3}
 	];
 
 
-	return {
-		getAll: function (){
-			return (pokemonList);
-		},
-		add: function (pokemon){
-			return pokemonList.push(pokemon)
-		}
+	function getAll(){
+		return pokemonList;
+	};
 
+	function add(pokemon) {
+		return pokemonList.push({pokemon})
+	};
+
+	return {
+		getAll: getAll,
+		add: add
 	};
 
 
 })();
 
-pokemonRepository.getAll().forEach(function getPokemonList(pokemon){
-	if (pokemon.height < 5){
-	document.write(pokemon.name + ' (height: ' + pokemon.height + '),' + ' type: ' + pokemon.type + '<br/>');
-} else if(pokemon.height > 5){
-	document.write(pokemon.name + ' (height: ' + pokemon.height + '),' + ' type: ' + pokemon.type  + ' -Wow, that\'s big!' + '<br/>')
-}
+pokemonRepository.getAll().forEach(function (pokemon){
+	if (pokemon.height > 4){
+	document.write(`${pokemon.name}, type:${pokemon.type}, height:${pokemon.height} - Wow that's big! </br>`);
+	} else {
+	document.write(`${pokemon.name}, type:${pokemon.type}, height:${pokemon.height}, </br>`)
+	}
 });
-
-console.log(pokemonRepository.getAll());
-
-
-
-
 
 
 
