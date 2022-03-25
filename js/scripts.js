57f92eb062bf0c74562454a4574211963b75b397
DarkFather63 <erynlcraig@gmail.com>
2022-03-25 15:15:18 -0700

    added eventListener and updated IIFE
    Organized code and added event listener functions, plus button functions. Updated css decoration a bit.

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,25 @@
 //creates a data array storing pokemon data
 //added IIFE function
+//created functions in IIFE that are globally accessible while limiting access 
 
 let pokemonRepository = (function(){
 
 	let pokemonList = [
-	{ name: ' Luxray ', type: ' electric', height: 2} ,
-	{ name: ' Zebstrika ', type: ' electric', height: 2} ,
-	{ name: ' Charizard', type: [' electric', ' flying'], height: 7} ,
-	{ name: ' Gengar', type: [' ghost', ' poison' ], height: 3}
+	{ name: ' Luxray ', 
+	type: ' electric', 
+	height: 2} ,
+
+	{ name: ' Zebstrika ', 
+	type: ' electric', 
+	height: 2} ,
+
+	{ name: ' Charizard', 
+	type: [' electric', ' flying'], 
+	height: 7} ,
+
+	{ name: ' Gengar', 
+	type: [' ghost', ' poison' ], 
+	height: 3}
 	];
 
 
@@ -16,23 +28,47 @@ let pokemonRepository = (function(){
 	};
 
 	function add(pokemon) {
-		return pokemonList.push({pokemon})
+		if (typeof pokemon === 'object' && 'name' in pokemon){
+			pokemonList.push(pokemon)
+		} else {
+			console.log('This is not the correct format for a pokemon.')
+		}
+	};
+
+	function showDetails(pokemon){
+		console.log(`name:${pokemon.name}, type:${pokemon.type}, height:${pokemon.height}`);
+	};
+
+	function addListener (button, pokemon){
+		button.addEventListener('click', function(){
+			showDetails(pokemon);
+		})
 	};
 
 	return {
 		getAll: getAll,
-		add: add
+		add: add,
+		addListItem: addListItem
 	};
+
+	function addListItem(pokemon) {
+		let pokemonUnorderedList = document.querySelector('.pokemon-list');
+		let unorderedListItem = document.createElement('li');
+		let button = document.createElement('button');
+		button.innerText = pokemon.name;
+		button.classList.add('button');
+		unorderedListItem.classList.add('list-item')
+		unorderedListItem.appendChild(button);
+		pokemonUnorderedList.appendChild(unorderedListItem);
+		addListener(button, pokemon);
+	}
 
 
 })();
 
 pokemonRepository.getAll().forEach(function (pokemon){
-	if (pokemon.height > 4){
-	document.write(`${pokemon.name}, type:${pokemon.type}, height:${pokemon.height} - Wow that's big! </br>`);
-	} else {
-	document.write(`${pokemon.name}, type:${pokemon.type}, height:${pokemon.height}, </br>`)
-	}
+
+	pokemonRepository.addListItem(pokemon);
 });
 
 
