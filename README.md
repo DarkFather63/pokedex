0863aa9c267241568b6f25d9e3bbb01317511c37
DarkFather63 <erynlcraig@gmail.com>
2022-04-12 11:04:40 -0700

    Formatting, linting and styling
    Added Prettier and eslint. Reformatted code. Added some personal styling preferences to the buttons and pages.

--- README.md
+++ README.md
@@ -1,11 +1,34 @@
+## Pokemon App
+
 This is a pokemon app!
 
-It's used based on a repository function as an IIFE. Within the IIFE are several functions that allow the app and users to access the details of pokemon, but don't allow disruption of the code that runs the app.
+###### DESCRIPTION:
+An app to be used as an index to find pokemon and retrieve some of their information. More information for pokemon will be added in the future.
 
-It also uses the pokemon open-source API, at https://pokeapi.co/api/v2/pokemon/?limit=150 . This only shows 150 pokemon - change the number after 'limit=' to any number if you want to see more or less. The pokemon are in the order written in the API, though, so it will only add or subtract those that are later in the API.
+My task was the development of this app using minimal HTML and CSS and mostly relying on Javascript. Javascript was entirely new to me and this app was meant to reflect my new knowledge. I had to create and understand javascript elements, DOM traversal, IIFEs, functions and base functionality.
 
-Pokemon can be added via the 'add' function included in the IIFE. They must follow the guidelines however.
+This app uses Javascript ES6 and later. 
 
-Some functions are purely for the look of the website, but they all add functionality. An important one, loadDetails, takes info from the API. Another, showDetails, allows users to click the buttons and receive the pokemon's info and image.
 
---This README is not yet finished - more to come soon!--
+###### USE:
+This app is used based on a repository function retrieving data from an API as an IIFE. Within the IIFE are several functions that allow the app and users to access the details of pokemon, but don't allow disruption of the code that runs the app. They are:
+
+**pokemonRepository** - The main IIFE - contains all the functions needed for the app to operate.
+
+**getAll** - Returns the pokemon list. NOTE: all functions are used in conjunction with each other - some are written very simply so that they can be called later in combination with other functions.
+
+**add** - Allows the addition of pokemon, primarily from the API. They are restricted based on formatting, particularly object formatting.
+
+**addListItem** - Adds elements to the DOM via bootstrap (see classes added and elements appended), mainly buttons and their content. It then calls on the showDetails function to load the pokemon details to a modal, which is in the showModal function further down in the code. It also adds some styling.
+
+**loadList** - Retrieves the pokemon list information from the pokeAPI. *This isn't referring to the list that we show in the app or reference in our DOM, merely the array and objects in the API.* It also uses dot notation to access the links to each pokemon's details.
+
+**loadDetails** - Refers to the loadList function variables and gets the details information from the API for each individual pokemon. This will be added to later, as it doesn't allow for pokemon types to be added yet. They need to be looped through first and returned separately.
+
+**showModal** - Using bootstrap's modal properties, constructs a modal to display the pokemon information. It also uses jQuery code.
+
+The API: A pokemon open-source API, at https://pokeapi.co/api/v2/pokemon/?limit=150 . This only shows 150 pokemon - change the number after 'limit=' to any number if you want to see more or less. The pokemon are in the order written in the API, though, so it will only add or subtract those that are later in the API.
+
+jQuery library: jquery-3.6.0.min.js (see files).
+
+Bootstrap is linked in the main html pages (bottom of the code, before the Javascript).
--- js/scripts.js
+++ js/scripts.js
@@ -1,148 +1,143 @@
-//created functions in IIFE that are globally accessible while limiting access 
+//created pokemon IIFE - it's long but accessible where needed
 
-let pokemonRepository = (function(){
+let pokemonRepository = (function() {
+  let pokemonList = [];
 
-	let pokemonList = [];
+  let apiUrl = "https://pokeapi.co/api/v2/pokemon/?limit=150";
 
-	let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
+  function getAll() {
+    return pokemonList;
+  }
 
-	function getAll(){
-		return pokemonList;
-	};
-
-//allows addition of pokemon - restricted if they don't fit the format
-	function add(pokemon) {
-		if (typeof pokemon === 'object' && 'name' in pokemon && 'detailsUrl' in pokemon){
-			pokemonList.push(pokemon)
-		} else {
-			console.log('This is not the correct format for a pokemon.')
-		}
-	};
-
-
-	function showDetails(pokemon){
-		loadDetails(pokemon).then(function(){
-			console.log(pokemon);
-			showModal(pokemon);
-		});
-	}
-
-//adds modal to each button with pokemon content, and allows for modal to be hidden and shown via button and click - uses bootstrap
-
-	function showModal(pokemon){
-		
-			let modalBody = $('.modal-body');
-			let modalTitle = $('.modal-title');
-			let modalHeader = $('.modal-header');
-
-			modalTitle.empty();
-			modalBody.empty();
-
-			let nameElement = $('<h1>'+ pokemon.name +'</h1>');
-
-			let pokemonImage = $('<img class="modal-img" style="width:50%">');
-
-			pokemonImage.attr('src', pokemon.imageUrl);
-
-			let heightElement = $('<p>'+'Height: '+pokemon.height+'</p>');
-
-			let weightElement = $('<p>'+'Weight: '+pokemon.weight+'</p>');
+  //allows addition of pokemon - restricted if they don't fit the format
+  function add(pokemon) {
+    if (
+      typeof pokemon === "object" &&
+      "name" in pokemon &&
+      "detailsUrl" in pokemon
+    ) {
+      pokemonList.push(pokemon);
+    } else {
+      console.log("This is not the correct format for a pokemon.");
+    }
+  }
 
 
-			modalTitle.append(nameElement);
-			modalBody.append(pokemonImage);
-			modalBody.append(heightElement);
-			modalBody.append(weightElement);
-		
+  //DOM manipulation - adds elements using bootstrap, appends them to elements on the dom, then applies a function
+	//to load the information from the showDetails function (which loads information from the API and shows the modal).
 
-			$('#pokemon-modal').modal('toggle')
+  function addListItem(pokemon) {
+    let pokemonUnorderedList = document.querySelector(".pokemon-list");
+    let unorderedListItem = document.createElement("li");
+    let button = document.createElement("button");
 
-	};
+    button.innerText = pokemon.name;
 
-//DOM manipulation - turns list into buttons and adds to it using variables, querySelector, createElement, and an event listener.
+    button.classList.add("button");
+    button.classList.add("btn");
+    button.classList.add("btn-primary");
+    button.classList.add("col");
 
-	function addListItem(pokemon) {
-		let pokemonUnorderedList = document.querySelector('.pokemon-list');
-		let unorderedListItem = document.createElement('li');
-		let button = document.createElement('button');
-		button.innerText = pokemon.name;
-		button.classList.add('button');
-		button.classList.add('btn');
-		button.classList.add('btn-primary');
-		unorderedListItem.classList.add('list-item');
-		unorderedListItem.appendChild(button);
-		unorderedListItem.classList.add('group-list-item');
-		pokemonUnorderedList.appendChild(unorderedListItem);
-		button.addEventListener('click', function(event){
-			showDetails(pokemon);
-		})
-	};
+    unorderedListItem.classList.add("list-item");
+    unorderedListItem.appendChild(button);
+    unorderedListItem.classList.add("group-list-item");
 
+    pokemonUnorderedList.appendChild(unorderedListItem);
 
-	//calls on the showDetails function above (which also has modal properties)
+    button.addEventListener("click", function(event) {
+      showDetails(pokemon);
+    });
+  }
 
-	// function addListener (button, pokemon){
-	// 	button.addEventListener('click', function(){
-	// 		showDetails(pokemon);
-	// 	});
-	// };
+  //fetches API info using json
 
-
-//fetches API info
-
-	function loadList() {
-	    return fetch(apiUrl).then(function (response) {
-	      return response.json();
-	    }).then(function (json) {
-	      json.results.forEach(function (item) {
-	        let pokemon = {
-	          name: item.name,
-	          detailsUrl: item.url
-	        };
-	        add(pokemon);
-	        console.log(pokemon);
-	      });
-	    }).catch(function (e) {
-	      console.error(e);
-	    })
-	  };
-
-//loads API, from url at beginning of code
-
-	function loadDetails(pokemon) {
-	    let url = pokemon.detailsUrl;
-	    return fetch(url).then(function (response) {
-	      return response.json();
-	    }).then(function (details) {
-	      pokemon.imageUrl = details.sprites.front_default;
-	      pokemon.height = details.height;
-	      pokemon.weight = details.weight;
-	    }).catch(function (e) {
-	      console.error(e);
-	    });
-	  };
-
-	return {
-		getAll: getAll,
-		add: add,
-		addListItem: addListItem,
-		loadList: loadList,
-		loadDetails: loadDetails,
-		showDetails: showDetails
-	};
+  function loadList() {
+    return fetch(apiUrl)
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(json) {
+        json.results.forEach(function(item) {
+          let pokemon = {
+            name: item.name,
+            detailsUrl: item.url
+          };
+          add(pokemon);
+          console.log(pokemon);
+        });
+      })
+      .catch(function(e) {
+        console.error(e);
+      });
+  }
 
 
+  //loads details from API, from url at beginning of code
+
+  function loadDetails(pokemon) {
+    let url = pokemon.detailsUrl;
+    return fetch(url)
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(details) {
+        pokemon.imageUrl = details.sprites.front_default;
+        pokemon.height = details.height;
+        pokemon.weight = details.weight;
+      })
+      .catch(function(e) {
+        console.error(e);
+      });
+  }
+
+
+  //adds modal to each button with pokemon content, and allows for modal to be hidden and shown via button and click - uses bootstrap
+
+  function showModal(pokemon) {
+    let modalBody = $(".modal-body");
+    let modalTitle = $(".modal-title");
+    let modalHeader = $(".modal-header");
+
+    modalTitle.empty();
+    modalBody.empty();
+
+    let nameElement = $("<h1>" + pokemon.name + "</h1>");
+
+    let pokemonImage = $('<img class="modal-img" style="width:50%">');
+
+    pokemonImage.attr("src", pokemon.imageUrl);
+
+    let heightElement = $("<p>" + "Height: " + pokemon.height + "</p>");
+
+    let weightElement = $("<p>" + "Weight: " + pokemon.weight + "</p>");
+
+    modalTitle.append(nameElement);
+    modalBody.append(pokemonImage);
+    modalBody.append(heightElement);
+    modalBody.append(weightElement);
+
+    $("#pokemon-modal").modal("toggle");
+  }
+
+  function showDetails(pokemon) {
+    loadDetails(pokemon).then(function() {
+      showModal(pokemon);
+    });
+  }
+
+  return {
+    getAll: getAll,
+    add: add,
+    addListItem: addListItem,
+    loadList: loadList,
+    loadDetails: loadDetails,
+    showDetails: showDetails
+  };
 })();
 
+//final calling of functions to execute repository
 pokemonRepository.loadList().then(function() {
-	pokemonRepository.getAll().forEach(function (pokemon){
-
-		pokemonRepository.addListItem(pokemon);
-	});
+  pokemonRepository.getAll().forEach(function(pokemon) {
+    pokemonRepository.addListItem(pokemon);
+  });
 });
-
-
-
-
-
-
